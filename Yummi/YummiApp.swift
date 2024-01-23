d48fe439e0fe287e71782fea4e57fc9efc397c0b
Josh Kim <kim.j@etoncollege.org.uk>
2024-01-23 09:58:09 +0000

    Initial Commit

---
+++ Yummi/YummiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YummiApp.swift
+//  Yummi
+//
+//  Created by Kim, Joshua (WING) on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct YummiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
