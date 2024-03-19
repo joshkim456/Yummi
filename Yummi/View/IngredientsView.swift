685462eaa552a8489a2461c6aac0d91686a16fd7
Josh Kim <kim.j@etoncollege.org.uk>
2024-03-19 11:12:42 +0000

    Inventory title

--- Yummi/View/IngredientsView.swift
+++ Yummi/View/IngredientsView.swift
@@ -12,27 +12,30 @@ struct IngredientsView: View {
     @State var ingredientsViewModel: IngredientsViewModel = IngredientsViewModel.shared
     
     var body: some View {
-        Form {
-            Section {
-                List {
-                    ForEach(ingredientsViewModel.ingredients, id: \.self.id) { ingredient in
-                        Text(ingredient.displayInformation())
+        NavigationStack {
+            Form {
+                Section {
+                    List {
+                        ForEach(ingredientsViewModel.ingredients, id: \.self.id) { ingredient in
+                            Text(ingredient.displayInformation())
+                        }
+                        .onDelete { indexSet in
+                            ingredientsViewModel.ingredients.remove(atOffsets: indexSet)
+                        }
                     }
-                    .onDelete { indexSet in
-                        ingredientsViewModel.ingredients.remove(atOffsets: indexSet)
+                }
+                Section {
+                    Button("Add Ingredient") {
+                        ingredientsViewModel.showingSheet.toggle()
+                    }
+                    .sheet(isPresented: $ingredientsViewModel.showingSheet) {
+                        Form {
+                            AddIngredientView(ingredientsViewModel: ingredientsViewModel)
+                        }
                     }
                 }
             }
-            Section {
-                Button("Add Ingredient") {
-                    ingredientsViewModel.showingSheet.toggle()
-                }
-                .sheet(isPresented: $ingredientsViewModel.showingSheet) {
-                    Form {
-                        AddIngredientView(ingredientsViewModel: ingredientsViewModel)
-                    }
-                }
-            }
+            .navigationTitle("Inventory")
         }
     }
 }
